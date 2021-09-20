aaa095b0c2d35c584844ec43ad3fed1e1588b32f
Christian Kirschberg <ckirschberg@gmail.com>
2021-09-20 10:18:38 +0200

    if else

--- src/Person.java
+++ src/Person.java
@@ -9,6 +9,7 @@ public class Person
     private boolean male; //note: male=true means a male, male=false means a female (SORRY all women!!)
     // (We learn a more intuitive way to do this later)
 
+
     public Person(String firstname, String lastname, int age, boolean male) {
         this.firstname = firstname;
         this.lastname = lastname;
@@ -20,43 +21,66 @@ public class Person
     //are for the personobject in question!
 
 
-    //The idea here is we want an answer to the question:
-    //Is this persons age 15? (true or false?)
     public boolean isAge(int age)
     {
-        //Your code here.
-        return true;
+        //return this.age == age;
+
+        if(age == this.age) {
+            return true;
+        }
+        return false;
     }
 
     //Code method that responds if name is the same as the parameter.
     public boolean IsPersonsName(String name)
     {
-        return true;
+        //String fullName = firstname + " " + lastname;
+        //if (fullName.equals(name)) {
+        if (name.equals(firstname + " " + lastname)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     //Here we want a method that responds true if the name of the personobject is NOT the same
     //as the parameter.
     public boolean IsPersonsNameNot(String name)
     {
-        return true;
+        //if (!name.equals(firstname + " " + lastname)) {
+        if (!IsPersonsName(name)) {
+            return true;
+        }
+        return false;
+
     }
 
     //Not operator with other expression. Age must match and name must not match to return true
     public boolean IsPersonsNameNotButAgeIs(String name, int age)
     {
-        return true;
+        if (age == this.age && IsPersonsNameNot(name)) {
+            return true;
+        }
+        return false;
+
     }
 
     //return true if name and age are the same as this person and otherwise false
     public boolean IsPersonAgeAndName(String name, int age)
     {
-        return true;
+        if (isAge(age) && IsPersonsName(name)) {
+            return true;
+        }
+        return false;
     }
 
     //Only return true if all 3 parameters (name, age and male) are the same as the person object
     public boolean IsPersonIdentical(String name, int age, boolean male)
     {
-        return true;
+        if (IsPersonAgeAndName(name, age) && this.male == male) {
+            return true;
+        }
+        return false;
     }
 
 
@@ -69,7 +93,24 @@ public class Person
     //age between 76 and up: return "Very Old"
     public String WhichAgeGroup()
     {
-        return "";
+
+        if (age >= 0 && age <= 5) {
+            return "Small Child";
+        } else if (age >= 6 && age <= 12) {
+            return "Child";
+        } else if (age >= 13 && age <= 19) {
+            return "Teenager";
+        } else if (age >= 20 && age <= 45) {
+            return "Adult";
+        } else if (age >= 46 && age <= 60) {
+            return "Mid age";
+        } else if (age >= 61 && age <= 75) {
+            return "Old";
+        } else if (age > 76 ){
+            return "Very Old";
+        }
+
+        return "Not Defined";
     }
 
 
@@ -80,6 +121,9 @@ public class Person
     // The suggestions (after you hit the . after object name, can also help you...
     public boolean IsPersonsNameCaseInsensitive(String name)
     {
-        return true;
+        if (name.equalsIgnoreCase(firstname + " " +  lastname)) {
+            return true;
+        }
+        return false;
     }
 }
